44f565cba50fa5fa36d932e71f7b2ccae15e74e3
dragos-constantin-stoica <dragos.constantin.stoica@outlook.com>
2023-06-30 07:19:02 +0200

    partial company page

--- ubill_app/public/js/company.js
+++ ubill_app/public/js/company.js
@@ -1,74 +1,106 @@
 Vue.component("company", {
-    data() {
-        return {
-            loading: true,
-            company: null,
-            company_list: [
-              { value: null, text: 'Please select an option' },
-              { value: 'a', text: 'This is First option' }
-            ],
-            show: true
-        }
-    },
+  data() {
+    return {
+      loading: true,
+      company: null,
+      company_list: [
+        { value: null, text: 'Please select an option' }
+      ],
+      show: true
+    }
+  },
 
-    created () {
-      axios
+  created() {
+    axios
       .get('/companies')
       .then(response => {
-         console.log(response.data)
-         if(response.data.status = 'ok'){
-            this.company_list = response.data.dataset.map(item => {
-                let tmp = {}
-                tmp.value = item
-                tmp.text = item.name
-                return tmp
-            })
-            this.company = this.company_list[0].value
-            this.loading=false
-         }
+        console.log(response.data)
+        if (response.data.status = 'ok') {
+          this.company_list = response.data.dataset.map(item => {
+            let tmp = {}
+            tmp.value = item
+            tmp.text = item.name
+            return tmp
+          })
+          this.company = this.company_list[0].value
+          this.loading = false
+        }
       })
-    },
+  },
 
-    template: `
-    <div v-if="loading">
+  template: `
+    <div class="d-flex justify-content-center mb-3" v-if="loading">
           <b-spinner type="grow" label="Loading..."></b-spinner>
     </div>
     <div v-else>
 
     <b-card title="Company" sub-title="Select one of the companies from the list">
         <b-form-select v-model="company" :options="company_list"></b-form-select>
-
-        <b-card-text>
-        Normaly it should be one single company
-        </b-card-text>
     </b-card>
 
     <b-card title="Details" header-tag="header" footer-tag="footer">
       <template #header>
         <h6 class="mb-0">Company data</h6>
       </template>
+
       <b-card-text>
       Mandatory Fields: name, national registration number, vat number, address
       </b-card-text>
-      <b-card-text>
-      Optional Fields: CUI, NORC EU ... specific to each country
-      </b-card-text>
+      
+      <b-form-group :label='$t("company.name")' label-for="company_name" label-cols-sm="3">
+        <b-form-input id="company_name" v-model="company.name"></b-form-input>
+      </b-form-group>
+
+      <b-form-group :label='$t("company.national_registration_number")' label-for="national_registration_number" label-cols-sm="3">
+        <b-form-input id="national_registration_number" v-model="company.national_registration_number"></b-form-input>
+      </b-form-group>
+
+      <b-form-group :label='$t("company.country")' label-for="country" label-cols-sm="3">
+        <b-form-input id="country" v-model="company.country"></b-form-input>
+      </b-form-group>
+
+      <b-form-group :label='$t("company.vat")' label-for="vat" label-cols-sm="3">
+        <b-form-input id="vat" v-model="company.vat"></b-form-input>
+      </b-form-group>
+
+      <b-form-group :label='$t("company.address")' label-for="address" label-cols-sm="3">
+        <b-form-input id="address" v-model="company.address"></b-form-input>
+      </b-form-group>
+
+
+      <b-form-group :label='$t("company.bank_accounts")' label-for="bank_accounts" label-cols-sm="3">
+        <b-form-input id="bank_accounts" v-model="company.bank_accounts"></b-form-input>
+      </b-form-group>
+      
       <b-card-text>
       Mandatory Fields - Bank accounts: Bank name, IBAN, SWIFT, BIC, Currency
       </b-card-text>
+
       <b-card-text>
       Mandatory Fields - Invoice number: year, series, number, format (standard format)
       </b-card-text>
+
       <b-card-text>
       Optional Fields - company logo
       </b-card-text>
+
       <b-card-text>
       Mandatory Fields - admin user, members users. Admin can manage admins and members. There is always one admin. The logged in user can not delete itself from the list if it is the only admin.
       </b-card-text>
-      <b-button href="#" variant="primary">Do something</b-button>
 
       <template #footer>
         <em>Fill in all mandatory fields</em>
+      </template>
+    </b-card>
+
+
+    <b-card title="Visual identity" header-tag="header" footer-tag="footer">
+      <template #header>
+        <h6 class="mb-0">Logo</h6>
+      </template>
+
+      <template #footer>
+        <em>Upload file 300 x 300 px</em>
       </template>
     </b-card>
 
--- ubill_app/public/js/messages.js
+++ ubill_app/public/js/messages.js
@@ -5,20 +5,20 @@ const messages = {
       hello: 'Hello!'
     },
     login: {
-     username: 'Username:',
-     password: 'Password:',
-     btn_login: 'Login',
-     btn_register: 'Register NEW Company'
+      username: 'Username:',
+      password: 'Password:',
+      btn_login: 'Login',
+      btn_register: 'Register NEW Company'
     },
     register: {
-     grp_user: 'Company Admin',
-     grp_company: 'Company data',
-     name: 'Full Company name:',
-     number: 'National registration number:',
-     country: 'Country',
-     btn_register: 'Register Company'
+      grp_user: 'Company Admin',
+      grp_company: 'Company data',
+      name: 'Full Company name:',
+      number: 'National registration number:',
+      country: 'Country',
+      btn_register: 'Register Company'
     },
-    mainmenu:{
+    mainmenu: {
       dashboard: 'Dashboard',
       company: 'Company',
       clients: 'Clients',
@@ -29,31 +29,39 @@ const messages = {
       profile: 'Profile',
       logout: 'Logout'
     },
-   profile: {
-     oldpassword: 'Current Password',
-     newpassword: 'New Password',
-     btn_changepassword: 'Change Password'
-   }
+    company:{
+      name: "Name",
+      national_registration_number: "National Registration Number",
+      country: "Country",
+      vat: "V.A.T",
+      address: "Address",
+      bank_accounts: "Bank Accounts"
+    },
+    profile: {
+      oldpassword: 'Current Password',
+      newpassword: 'New Password',
+      btn_changepassword: 'Change Password'
+    }
   },
   fr: {
     message: {
       hello: 'Bonjour!'
     },
     login: {
-     username: 'Nom d\'utilisateur :',
-     password: 'Mot de passe :',
-     btn_login: 'Connexion',
-     btn_register: 'Enregister NOUVELLE Entreprise'
-   },
-    register: {
-     grp_user: 'Compte Administrateur',
-     grp_company: 'Données Entreprise',
-     name: 'Nom complèt:',
-     number: 'Numèro National:',
-     country: 'Pays',
-     btn_register: 'Enregistrer Enteprise'
+      username: 'Nom d\'utilisateur :',
+      password: 'Mot de passe :',
+      btn_login: 'Connexion',
+      btn_register: 'Enregister NOUVELLE Entreprise'
     },
-    mainmenu:{
+    register: {
+      grp_user: 'Compte Administrateur',
+      grp_company: 'Données Entreprise',
+      name: 'Nom complèt:',
+      number: 'Numèro National:',
+      country: 'Pays',
+      btn_register: 'Enregistrer Enteprise'
+    },
+    mainmenu: {
       dashboard: 'Tableau de Bord',
       company: 'Compagnie',
       clients: 'Clients',
@@ -64,31 +72,39 @@ const messages = {
       profile: 'Profile',
       logout: 'Deconnection'
     },
-   profile: {
-     oldpassword: 'Mot de passe actuel',
-     newpassword: 'Nouveau mot de passe',
-     btn_changepassword: 'Changer le mot de passe'
-   }
+    company:{
+      name: "Nom",
+      national_registration_number: "Numero National",
+      country: "Pays",
+      vat: "T.V.A",
+      address: "Adress",
+      bank_accounts: "Comptes bancaires"
+    },
+    profile: {
+      oldpassword: 'Mot de passe actuel',
+      newpassword: 'Nouveau mot de passe',
+      btn_changepassword: 'Changer le mot de passe'
+    }
   },
   ro: {
     message: {
       hello: 'Bine ati venit!'
     },
     login: {
-     username: 'Utilizator :',
-     password: 'Parola :',
-     btn_login: 'Conectare',
-     btn_register: 'Enregistati Companie NOUA'
-   },
-    register: {
-     grp_user: 'Cont Administrator',
-     grp_company: 'Date Companie',
-     name: 'Nume complet:',
-     number: 'Numar National:',
-     country: 'Tara',
-     btn_register: 'Enregistreaza Companie'
+      username: 'Utilizator :',
+      password: 'Parola :',
+      btn_login: 'Conectare',
+      btn_register: 'Enregistati Companie NOUA'
     },
-    mainmenu:{
+    register: {
+      grp_user: 'Cont Administrator',
+      grp_company: 'Date Companie',
+      name: 'Nume complet:',
+      number: 'Numar National:',
+      country: 'Tara',
+      btn_register: 'Enregistreaza Companie'
+    },
+    mainmenu: {
       dashboard: 'Tablou de Bord',
       company: 'Companie',
       clients: 'Clienti',
@@ -99,10 +115,18 @@ const messages = {
       profile: 'Profil',
       logout: 'Deconectare'
     },
-   profile: {
-     oldpassword: 'Parola curenta',
-     newpassword: 'Noua Parola',
-     btn_changepassword: 'Schimba parola'
-   }
+    company:{
+      name: "Nume",
+      national_registration_number: "Numarul National de Inregistrare",
+      country: "Tara",
+      vat: "Numar T.V.A",
+      address: "Adresa",
+      bank_accounts: "Conturile bancare"
+    },
+    profile: {
+      oldpassword: 'Parola curenta',
+      newpassword: 'Noua Parola',
+      btn_changepassword: 'Schimba parola'
+    }
   }
 }

435fba1a4ec4933689b0fa464cee4c090496dc88
dragos-constantin-stoica <dragos.constantin.stoica@outlook.com>
2023-09-06 19:11:42 +0200

    generator for communication structuree - close #27

--- ubill_app/public/js/utils.js
+++ ubill_app/public/js/utils.js
@@ -23,3 +23,16 @@ function showToast(message, title='Message from server', type='info'){
     appendToast: true
    })
 }
+
+//Get the day of the year
+function getDay(date = new Date()) {
+  return Math.floor((date - new Date(date.getFullYear(), 0, 0)) / (1000 * 60 * 60 * 24));
+}
+
+//Get Communication Structuree
+function getCS(date=new Date(), invoice_number) {
+  //The format is +++DDD/YYYY/####CC+++
+  const fy = date.getFullYear(), ddd = getDay(date), magic_number = parseInt("".concat(ddd, fy, invoice_number.toString()).padStart(4,'0'), 10)
+  let cc = (magic_number%97 == 0)? 97:(magic_number%97)
+  return `+++${ddd.toString().padStart(3,'0')}/${fy.toString().padStart(4,'0')}/${invoice_number.toString().padStart(4,'0')}${cc.toString().padStart(2,'0')}+++`  
+}

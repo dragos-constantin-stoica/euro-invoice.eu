b58dcd50640eb1321f842729f5aee73795abd101
dragos-constantin-stoica <dragos.constantin.stoica@outlook.com>
2023-09-11 14:42:18 +0000

    full setup script

--- dsb.sh
+++ dsb.sh
@@ -12,7 +12,7 @@
 #
 # @company: DataStema Sarl
 # @date: 01.02.2023
-# @version: 2.9.0
+# @version: 3.1.4
 # @author: dragos.stoica@datastema.io
 #----------------------------------------------------------------------------
 
@@ -23,8 +23,8 @@ source .env
 usage(){
     local __usage="Usage:
     -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~
-    $0 setup
-        setup each container
+    $0 setup [ssl | null]
+        setup each container with ssl or without ssl (for dev environment)
     $0 ssl
         setup SSL certificates from Lets Encrypt via Cerbot
     $0 redeploy [service_name]
@@ -52,9 +52,9 @@ where [service_name] is:
 
 # setup function
 setup(){
-    echo "Setup stage"
+    echocolor "Setup stage" "Yellow"
     #create docker folders
-    local FOLDERS=( "./dbcouch" "./dbcouch/data" "./dbcouch/etc" "./dbcouch/log" "./nginx" "./nginx/conf")
+    local FOLDERS=( "./dbcouch" "./dbcouch/data" "./dbcouch/etc" "./dbcouch/log")
     for i in "${FOLDERS[@]}"
     do
 	if [ ! -d "$i" ]; then
@@ -85,11 +85,10 @@ setup(){
 }
 EOF
 
-    function setting {
+    setting(){
         local NODE_NAME="nonode@nohost"
-        echo
         echo "setting: $1 $2 \"$3\""
-        echo -n "   prev: "
+        COUCH_URL=http://$COUCHDB_USER:$COUCHDB_PASSWORD@couch.localhost:5984
         curl -H 'Content-Type: application/json' -X PUT $COUCH_URL/_node/$NODE_NAME/_config/$1/$2 -d "\"$3\""
     }
 
@@ -137,11 +136,29 @@ EOF
     sleep 10
     docker compose down
 
-    echo "DONE >>> Setup stage"
+    if [[ ! -z "$1" ]]; then
+    	case $1 in
+    		"ssl") 
+    		    echocolor "Setup SSL" "Yellow"
+    			cp nginx/conf/nginx_setup nginx/conf/nginx.conf
+			    docker compose up -d
+			    sleep 60
+			    certbotssl
+			    sleep 60
+			    docker compose down
+			    cp nginx/conf/nginx_ssl nginx/conf/nginx.conf
+			    echocolor "SSL setup done!" "Yellow";;
+
+    		*) echo "Unknown paramenter $1"
+    		   usage;;
+    	esac
+    fi
+
+    echocolor "DONE >>> Setup stage" "Yellow"
 }
 
 # certbot ssl management
-cerbotssl(){
+certbotssl(){
     echo "Setup SSL certificates from Lets Encrypt using Certbot"
     local FOLDERS=( "./certbot" "./certbot/www" "./certbot/conf")
     for i in "${FOLDERS[@]}"
@@ -164,7 +181,7 @@ redeploy(){
 
 # clean up all folders
 cleanup(){
-    echo "Cleanup stage"
+    echocolor "Cleanup stage" "Red"
     echo "Do you want to delete ALL data?"
     select yn in "Yes" "No"
     do
@@ -185,12 +202,12 @@ cleanup(){
         esac
     done
 
-    echo "DONE >>> Cleanup stage"
+    echocolor "DONE >>> Cleanup stage" "Red"
 }
 
 # run function
 run(){
-    echo "Run stage"
+    echocolor "Run stage" "Green"
     docker compose up -d
 
     echo "CouchDB has successfuly started on http://couch.localhost:5984/_utils"
@@ -198,7 +215,8 @@ run(){
     echo "Couch Admin Worker have successfully started on http://localhost:$WRK_PORT"
     echo "UnityBill has successfully started on http://localhost:8080"
     echo "Application available at https://$CERT_DOMAIN"
-    echo -e "\n\nDONE >>> Run stage"
+    echo -e "\n\n"
+    echocolor "DONE >>> Run stage" "Green"
 }
 
 # dev function
@@ -235,6 +253,94 @@ build(){
    echo "DONE >>> build service $1"
 }
 
+
+# Screen output nice colors
+declare -A colors
+# Reset
+colors[Color_Off]='\033[0m'       # Text Reset
+
+# Regular Colors
+colors[Black]='\033[0;30m'        # Black
+colors[Red]='\033[0;31m'          # Red
+colors[Green]='\033[0;32m'        # Green
+colors[Yellow]='\033[0;33m'       # Yellow
+colors[Blue]='\033[0;34m'         # Blue
+colors[Purple]='\033[0;35m'       # Purple
+colors[Cyan]='\033[0;36m'         # Cyan
+colors[White]='\033[0;37m'        # White
+
+# Bold
+colors[BBlack]='\033[1;30m'       # Black
+colors[BRed]='\033[1;31m'         # Red
+colors[BGreen]='\033[1;32m'       # Green
+colors[BYellow]='\033[1;33m'      # Yellow
+colors[BBlue]='\033[1;34m'        # Blue
+colors[BPurple]='\033[1;35m'      # Purple
+colors[BCyan]='\033[1;36m'        # Cyan
+colors[BWhite]='\033[1;37m'       # White
+
+# Underline
+colors[UBlack]='\033[4;30m'       # Black
+colors[URed]='\033[4;31m'         # Red
+colors[UGreen]='\033[4;32m'       # Green
+colors[UYellow]='\033[4;33m'      # Yellow
+colors[UBlue]='\033[4;34m'        # Blue
+colors[UPurple]='\033[4;35m'      # Purple
+colors[UCyan]='\033[4;36m'        # Cyan
+colors[UWhite]='\033[4;37m'       # White
+
+# Background
+colors[On_Black]='\033[40m'       # Black
+colors[On_Red]='\033[41m'         # Red
+colors[On_Green]='\033[42m'       # Green
+colors[On_Yellow]='\033[43m'      # Yellow
+colors[On_Blue]='\033[44m'        # Blue
+colors[On_Purple]='\033[45m'      # Purple
+colors[On_Cyan]='\033[46m'        # Cyan
+colors[On_White]='\033[47m'       # White
+
+# High Intensity
+colors[IBlack]='\033[0;90m'       # Black
+colors[IRed]='\033[0;91m'         # Red
+colors[IGreen]='\033[0;92m'       # Green
+colors[IYellow]='\033[0;93m'      # Yellow
+colors[IBlue]='\033[0;94m'        # Blue
+colors[IPurple]='\033[0;95m'      # Purple
+colors[ICyan]='\033[0;96m'        # Cyan
+colors[IWhite]='\033[0;97m'       # White
+
+# Bold High Intensity
+colors[BIBlack]='\033[1;90m'      # Black
+colors[BIRed]='\033[1;91m'        # Red
+colors[BIGreen]='\033[1;92m'      # Green
+colors[BIYellow]='\033[1;93m'     # Yellow
+colors[BIBlue]='\033[1;94m'       # Blue
+colors[BIPurple]='\033[1;95m'     # Purple
+colors[BICyan]='\033[1;96m'       # Cyan
+colors[BIWhite]='\033[1;97m'      # White
+
+# High Intensity backgrounds
+colors[On_IBlack]='\033[0;100m'   # Black
+colors[On_IRed]='\033[0;101m'     # Red
+colors[On_IGreen]='\033[0;102m'   # Green
+colors[On_IYellow]='\033[0;103m'  # Yellow
+colors[On_IBlue]='\033[0;104m'    # Blue
+colors[On_IPurple]='\033[0;105m'  # Purple
+colors[On_ICyan]='\033[0;106m'    # Cyan
+colors[On_IWhite]='\033[0;107m'   # White
+
+echocolor(){
+    # $1 - the text, $2 - color
+    color=${colors[White]}
+    if [[ ! -z "$2" ]]; then
+    	color=${colors[$2]}
+    fi
+
+	echo -e "\U1F916 $color $1 ${colors[Color_Off]}"
+}
+
+# end of screen colors
+
 ##############################################################################
 # main script
 
@@ -246,8 +352,8 @@ fi
 #echo $1, $2, $3, $4
 
 case $1 in
-    "setup")    setup ;;
-    "ssl")      cerbotssl ;;
+    "setup")    setup $2;;
+    "ssl")      certbotssl ;;
     "cleanup")  cleanup ;;
     "redeploy") redeploy $2;;
     "run")      run ;;
@@ -256,7 +362,7 @@ case $1 in
     "build")    build $2;;
     "prune")    prune;;
     "usage")    usage ;;
-    *)      	echo "unknown command: $1"
+    *)      	echocolor "unknown command: $1" On_IRed
 	            usage ;;
 esac
 
